06dd0ffffe26cb2fcabb5e0f0b2ec9f0e324e350
Kayshi810 <78273109+Kayshi810@users.noreply.github.com>
2021-10-14 18:59:24 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 song="";
 song_2="";
+left_wrist_x=0;
+left_wrist_y=0;
+right_wrist_x=0;
+right_wrist_y=0;
 function preload()
 {
     song=loadSound("music.mp3");
-    song_2=loadSound("music2.mp3");
+    song=loadSound("music2.mp3");
 }
 
 function play()
 {
 song.play();
+song.setVolume(1);
+song.rate(1);
+
 }
 
 function setup()
@@ -17,8 +24,28 @@ function setup()
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose', gotPoses);
 }
 
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        left_wrist_x=results[0].pose.leftWrist.x;
+        left_wrist_y=results[0].pose.leftWrist.y;
+        console.log("Left_Wrist_X = "+left_wrist_x+", Left_Wrist_Y = "+left_wrist_y);
+        right_wrist_x=results[0].pose.rightWrist.x;
+        right_wrist_y=results[0].pose.rightWrist.y;
+        console.log("Right_Wrist_X = "+right_wrist_x+", Right_Wrist_Y = "+right_wrist_y);
+    }
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet is Initialized");
+}
 function draw()
 {
     image(video, 0, 0, 600, 500);
